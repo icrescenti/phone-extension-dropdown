eee04616b48140b8e6e1f00763ffbb765098f9ec
icrescenti <icrescenti77@gmail.com>
2025-03-25 09:39:51 +0100

    demo initial commit

---
+++ demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RegistrationDemo from './RegistrationDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationDemo />
+    </div>
+  );
+}
+
+export default App;
