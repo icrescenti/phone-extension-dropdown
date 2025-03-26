9c8781ae0d79dd5da827f5b88d00d6fe251f6705
icrescenti <icrescenti77@gmail.com>
2025-03-26 16:39:25 +0100

    extended demo, validation added

--- demo/src/RegistrationDemo.js
+++ demo/src/RegistrationDemo.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './RegistrationDemo.css';
-import PhoneExtensionDropdown from 'phone-extension-dropdown';
+import PhoneExtensionDropdown, { validatePhone } from 'phone-extension-dropdown';
 
 function RegistrationDemo() {
+	
+	const [phone, setPhone] = useState('');	
+	const [countryCode, setCountryCode] = useState('US');
+	const [validPhone, setValidPhone] = useState(false);
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		alert('This page is just a demo');
+		alert('Phone is valid: ' + validatePhone(countryCode, phone).toString());
 	};
+
+	useEffect(() => {
+		setValidPhone(validatePhone(countryCode, phone));
+	}, [countryCode, phone]);
 
 	return (
 		<div className="registration-container">
 			<div className="registration-card">
 				<h1>Demo</h1>
 				<div>
-					<div style={{marginBottom: "20px"}}>
+					<div>
 					<PhoneExtensionDropdown
+						language="en"
 						TextFieldAddition={
 							<div className="input-group" style={{marginBottom: "0px"}}>
 								<input
@@ -22,14 +32,20 @@ function RegistrationDemo() {
 									id="username"
 									name="username"
 									style={{width: '100%', height: '4rem'}}
+									value={phone}
+									onChange={(e) => setPhone(e.target.value)}
 								/>
 							</div>
 						}
+						onSelect={(code, countryCode) => {
+							setCountryCode(countryCode);
+						}}
 						DropdownStyle={{
 							color: "#e0e0e0"
 						}}
 					/>
 					</div>
+					<span style={{fontSize: '0.9rem', color: validPhone ? 'green' : 'red', marginBottom: "20px"}}>{validPhone ? 'Valid phone' : 'Invalid phone'}</span>
 					<div className="input-group">
 						<label htmlFor="password">Password</label>
 						<input
